e8b29e8162861e5b8ce1c18a560cff2680bbf42d
Mariano Deville <mariano_deville@hotmail.com>
2025-09-11 21:14:00 -0300

    probando

--- inc/leds.h
+++ inc/leds.h
@@ -3,8 +3,8 @@
 
 #include <stdint-gcc.h>
 
-#define ALL_LED_OFF         0X0000
-#define ALL_LED_ON          ~ALL_LED_OFF
+#define ALL_LED_OFF 0X0000
+#define ALL_LED_ON  ~ALL_LED_OFF
 
 typedef enum {
 
